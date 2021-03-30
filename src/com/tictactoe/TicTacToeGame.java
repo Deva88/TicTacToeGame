74d1a8eb6846b696317033253d882f1dfb87c6e9
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-30 13:54:21 +0530

    Added: creating a tic tac toe game

--- src/com/tictactoe/TicTacToeGame.java
+++ src/com/tictactoe/TicTacToeGame.java
@@ -1,10 +1,21 @@
 package com.tictactoe;
 
-public class TicTacToeGame{
-	
+/*@Description- simulator for TicTacToe Game*/
+public class TicTacToeGame {
+
+	/* UC1 -- Creating a Board */
+	public static char[] createBoard() {
+		char[] board = new char[10];
+		/* initialize board with empty space */
+		for (int pos = 1; pos < board.length; pos++) {
+			board[pos] = ' ';
+		}
+		return board;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the TicTacToe Game");
-	
+		createBoard();
 	}
 
 }
