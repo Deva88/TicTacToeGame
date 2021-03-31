374e6ef183fd44f99182cc6014f09af892875ff0
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-01 03:04:59 +0530

    Added: Check for the free space before making the desired move

--- src/com/tictactoe/TicTacToeGame.java
+++ src/com/tictactoe/TicTacToeGame.java
@@ -1,5 +1,5 @@
 package com.tictactoe;
-
+import java.util.*;
 /*@Description- simulator for TicTacToe Game show Board*/
 
 public class TicTacToeGame {
@@ -12,6 +12,40 @@ public class TicTacToeGame {
 						   { '-', '+', '-', '+', '-' }, 
 						   { ' ', '|', ' ', '|', ' ' }, //row 
 						   };
+		System.out.println("Enter the position in between 1 to 9 ");
+	
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		//Using switch 
+		switch(n) {
+		case 1:
+			board [0][0]='X';
+			break;
+		case 2:
+			board [0][2]='X';
+			break;
+		case 3:
+			board [0][4]='X';
+			break;
+		case 4:
+			board [2][0]='X';
+			break;
+		case 5:
+			board [2][2]='X';
+			break;
+		case 6:
+			board [2][4]='X';
+			break;
+		case 7:
+			board [4][0]='X';
+			break;
+		case 8:
+			board [4][2]='X';
+			break;
+		case 9:
+			board [4][4]='X';
+			break;
+		}
 		//catch character in for loop
 		for (int i = 0; i < board.length; i++) {
 			System.out.println(board[i]);
@@ -20,6 +54,7 @@ public class TicTacToeGame {
 	
 	public static void main(String[] args) {
 		showBoard();
+		
 	}
 
 }
