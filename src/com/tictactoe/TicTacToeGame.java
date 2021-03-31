744a71138819a6e203da93deee24f278febe351d
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-31 20:02:54 +0530

    Added: Player to input X or O and Determine player and Computer

--- src/com/tictactoe/TicTacToeGame.java
+++ src/com/tictactoe/TicTacToeGame.java
@@ -1,22 +1,35 @@
 package com.tictactoe;
 
+import java.util.*;
 /*@Description- simulator for TicTacToe Game*/
 
 public class TicTacToeGame {
 
-	// create one method board()
-	public static void board() {
-		// create the board of size 10
-		char[] board = new char[10];
-		// initialize board with empty space and ignore 0th index
-		for (int pos = 1; pos < board.length; pos++) {
-			board[pos] = ' ';
+	static Scanner sc = new Scanner(System.in);
+	static char playerLetter, computerLetter;
+
+	// Taking Input
+	public static char takeInput() {
+		System.out.println("Enter the letter you wish to take: ('X' / 'O'): ");
+		char input = Character.toUpperCase(sc.next().charAt(0));
+		if (input == 'X' || input == 'O') {
+			setLetter(input);
+			return input;
+		} else {
+			System.out.println("Invalid Character. Try Again.");
+			return takeInput();
 		}
 	}
 
-	public static void main(String[] args) {
-		// call method
-		board();
+	// Determine letter for player and computer
+	public static void setLetter(char playerLetter) {
+		computerLetter = (playerLetter == 'X') ? 'O' : 'X';
+		System.out.println("Player Letter: " + playerLetter + "\nComputer Letter : " + computerLetter);
 	}
 
+	public static void main(String args[]) {
+		System.out.println("Welcome to the TIC_TAC_TOE game");
+		// userInput();
+		takeInput();
+	}
 }
